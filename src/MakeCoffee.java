2dbd59428fbb2ec846d375c6d4a06e85ec690e45
Won Jung <won.jung@pacific.costcotravel.com>
2018-08-02 14:34:39 -0700

    cafe

---
+++ src/MakeCoffee.java
@@ -0,0 +1,5 @@
+public class MakeCoffee {
+    public static void main(String args[]) {
+
+    }
+}
---
+++ src/com/kokoloko/machine/Roaster/RoastMachine.java
@@ -0,0 +1,18 @@
+package com.kokoloko.machine.Roaster;
+
+import com.kokoloko.coffee.beans.Bean;
+
+public class RoastMachine {
+    Bean bean;
+    public RoastMachine(Bean bean) {
+        this.bean = bean;
+    }
+
+    public void Roast() {
+
+    }
+
+    public Bean getBean() {
+        return bean;
+    }
+}
